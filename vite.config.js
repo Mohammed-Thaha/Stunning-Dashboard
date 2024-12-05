2f418ddf9527aebdce6f957d483ecbb93d327c1b
Mohammed-Thaha <mohammedthahacse@gmail.com>
2024-12-05 16:31:16 +0530

    update changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/https://github.com/Mohammed-Thaha/Stunning-Dashboard.git/', // Replace <repository-name> with your GitHub repo name
+  base: '/Stunning-Dashboard/', // Replace <repository-name> with your GitHub repo name
   plugins: [
     react({
       fastRefresh: false, // Disable Fast Refresh (optional workaround)
